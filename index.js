2c47204ad8e3993824a13cdf2126912ef98db661
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-01 14:22:00 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let end = (word.length - (word.length % 2)) / 2;
+
+  for (let i = 0; i < end; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
